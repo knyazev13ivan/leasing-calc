eed7255820619aa62367123a2ab0ad8c085b2e63
Ivan Knyazev <knyazev13ivan@gmail.com>
2022-10-05 03:20:02 +0300

    update request url

--- src/store/base.api.ts
+++ src/store/base.api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://eoj3r7f3r4ef6v4.m.pipedream.net",
+    baseUrl: "https://hookb.in/eK160jgYJ6UlaRPldJ1P",
   }),
   endpoints: () => ({}),
 });
